071c2fb9f766026682e45b4580824e403d742a73
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2024-07-12 19:30:57 +0530

    wd401 L7 docker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m44qpkdnph8"
+    "revision": "0.644ddj3ld"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
