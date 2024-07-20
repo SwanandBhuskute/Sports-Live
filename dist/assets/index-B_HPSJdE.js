7b29b0c947c4de14d552c99692132424ecb1114d
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2024-07-20 19:52:12 +0530

    wd401 L8 done

--- dist/assets/index-B_HPSJdE.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TeamandSportList-C0IxxeLQ.js","assets/index-B2aDYPK_.js","assets/index-BbyOO-Tw.css","assets/constants-C9Jc7q6O.js","assets/NavBar-BHJ-zrLT.js","assets/dateUtils-gG17I_SJ.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{R as r,_ as a,j as s,r as e,O as t}from"./index-B2aDYPK_.js";import{E as o}from"./ErrorBoundary-NlOW2PwI.js";const n=r.lazy(()=>a(()=>import("./TeamandSportList-C0IxxeLQ.js"),__vite__mapDeps([0,1,2,3,4,5]))),l=()=>s.jsxs(s.Fragment,{children:[s.jsx(o,{children:s.jsx(e.Suspense,{fallback:s.jsx("div",{className:"suspense-loading",children:"Loading..."}),children:s.jsx(n,{})})}),s.jsx(t,{})]});export{l as default};
